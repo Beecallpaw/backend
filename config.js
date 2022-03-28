c46ad56a65f75562648cc47341fc1908efe64420
bikalpa-t <bikalpa.t@homeloanexperts.com.au>
2022-03-28 13:31:30 +0545

    heroku updates

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 module.exports = {
     database: {
-        dbDSN: process.env.dbDSN ? process.env.dbDSN : "mongodb://localhost:27017/assessment"
+        dbDSN: process.env.dbDSN ? process.env.dbDSN : "mongodb+srv://beecallpaw:tr6s8ZWgWCAgv6fc@cluster0.gicg5.mongodb.net/assessment?authSource=admin&replicaSet=atlas-aoa2w4-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
     },
     port: process.env.port ? process.env.port : "4000"
 }
